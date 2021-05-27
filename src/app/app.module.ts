edd4d4d02399fcf1b440d5fe54a38f83a84f1554
OlehBabiak <oleg_bob@ukr.net>
2021-05-28 01:20:53 +0300

    home-work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './components/users/users.component';
 import {HttpClientModule} from "@angular/common/http";
-import { UserComponent } from './components/user/user.component';
+import { UserComponent } from './components/users/user/user.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { PostComponent } from './components/posts/post/post.component';
+import { AlbumsComponent } from './components/albums/albums.component';
+import { AlbumComponent } from './components/albums/album/album.component';
+import { TodosComponent } from './components/todos/todos.component';
+import { TodoComponent } from './components/todos/todo/todo.component';
+import { CommentsComponent } from './components/posts/post/comments/comments.component';
+import { CommentComponent } from './components/posts/post/comments/comment/comment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
-    UserComponent
+    UserComponent,
+    PostsComponent,
+    PostComponent,
+    AlbumsComponent,
+    AlbumComponent,
+    TodosComponent,
+    TodoComponent,
+    CommentsComponent,
+    CommentComponent
   ],
   imports: [
     BrowserModule,
